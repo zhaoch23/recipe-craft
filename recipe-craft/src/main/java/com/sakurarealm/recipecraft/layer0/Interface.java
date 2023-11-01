3436803d3911a3e3e1051cef78f3253aa9c15a77
StarryDust <44486240+StarryDust-02@users.noreply.github.com>
2023-11-01 01:40:44 -0400

    Update Interface

--- recipe-craft/src/main/java/com/sakurarealm/recipecraft/layer0/Interface.java
+++ recipe-craft/src/main/java/com/sakurarealm/recipecraft/layer0/Interface.java
@@ -2,12 +2,7 @@ package com.sakurarealm.recipecraft.layer0;
 
 public abstract interface Interface {
 
-    public static void main(String name, String id) {
-        String objectName = name;
-        String playerId = id;
-    }
-
-    public default void identifyState() {
+    public default void identifyState(String objectName, String playerId) {
 
     }
 
