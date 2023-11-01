4db59c1dc5b13787da3c532ba1b2371fe694c3b6
zhaoch23 <chengzong.zhao@gmail.com>
2023-11-01 01:25:07 -0400

    Add Bukkit plugin

--- recipe-craft/src/main/java/com/sakurarealm/recipecraft/client/Main.java
+++ recipe-craft/src/main/java/com/sakurarealm/recipecraft/client/Main.java
@@ -1,4 +1,4 @@
-package com.sakurarealm.recipecraft;
+package com.sakurarealm.recipecraft.client;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
