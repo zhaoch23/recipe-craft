47342234775fe887bbf4e96637c9e8a0d42ed2a0
StarryDust <44486240+StarryDust-02@users.noreply.github.com>
2023-11-01 01:34:45 -0400

    Abstract Interface

--- recipe-craft/src/main/java/com/sakurarealm/recipecraft/layer0/Interface.java
+++ recipe-craft/src/main/java/com/sakurarealm/recipecraft/layer0/Interface.java
@@ -1,8 +1,15 @@
 package com.sakurarealm.recipecraft.layer0;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public abstract interface Interface {
 
-    
+    public static void main(String name, String id) {
+        String objectName = name;
+        String playerId = id;
+    }
+
+    public default void identifyState() {
+
+    }
+
+
 }
