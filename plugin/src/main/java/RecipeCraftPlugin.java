ca46e6f7d1e5f9271cc162d4055caba3096125dd
zhaoch23 <c233zhao@uwaterloo.ca>
2023-11-03 21:13:20 -0400

    Refactor project structure

---
+++ plugin/src/main/java/RecipeCraftPlugin.java
@@ -0,0 +1,15 @@
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class RecipeCraftPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+}
